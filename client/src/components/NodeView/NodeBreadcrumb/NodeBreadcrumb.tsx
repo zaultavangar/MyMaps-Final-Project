59972587ceed4b046c29bfd6ac0a4c28eca1f574
Robert Koch <robert_koch@brown.edu>
2022-12-12 22:42:38 -0500

    fix: breadcrumb bugs

--- client/src/components/NodeView/NodeBreadcrumb/NodeBreadcrumb.tsx
+++ client/src/components/NodeView/NodeBreadcrumb/NodeBreadcrumb.tsx
@@ -14,6 +14,7 @@ import { useSetRecoilState } from 'recoil'
 import { selectedNodeState } from '../../../global/Atoms'
 import { Link } from 'react-router-dom'
 import { IPin } from '../../../types'
+import { Spinner } from '@chakra-ui/react'
 
 export interface INodeBreadcrumbProps {
   nodeIdsToNodesMap: NodeIdsToNodesMap
@@ -35,24 +36,20 @@ export const NodeBreadcrumb = ({
   const currentNode = pathNodes[pathNodes.length - 1]
   const setSelectedNode = useSetRecoilState(selectedNodeState)
 
+  // get this node's pin
+  const pin = pinIdsToPinsMap[currentNode.pinId]
+
   // get the node of type map in the pathNodes, if it exists
   const mapNode = pathNodes.find((node) => node.type === 'map')
 
   return (
     <div className="node-breadcrumb">
       {parentNodes.map((node: INode) => (
-        <Link to={`/${pathToString(node.filePath)}`} key={node.nodeId}>
-          {/* {node.type !== 'map' && (
-            <div className="breadcrumb-item-wrapper">
-              <div
-                className={'breadcrumb-item'}
-                onClick={() => setSelectedNode(mapNode || node)}
-              >
-                {pinIdsToPinsMap[node.pinId]?.title || 'Pin'}
-              </div>
-              <RiArrowRightSLine />
-            </div>
-          )} */}
+        <Link
+          to={`/${pathToString(node.filePath)}`}
+          key={node.nodeId}
+          style={{ display: 'flex' }}
+        >
           <div className="breadcrumb-item-wrapper">
             <div className={'breadcrumb-item'} onClick={() => setSelectedNode(node)}>
               <div className="item-icon">
@@ -65,29 +62,24 @@ export const NodeBreadcrumb = ({
             </div>
             <RiArrowRightSLine />
           </div>
+          <div>
+            {node.type === 'map' && pin && pin.nodeId == node.nodeId && (
+              <div className="breadcrumb-item-wrapper">
+                <div
+                  className={'breadcrumb-item'}
+                  onClick={() => setSelectedNode(mapNode || node)}
+                >
+                  <div className="item-icon">
+                    <RiMapPinLine />
+                  </div>
+                  {pin?.title || <Spinner size="xs" />}
+                </div>
+                <RiArrowRightSLine />
+              </div>
+            )}
+          </div>
         </Link>
       ))}
-      <div>
-        {currentNode.type !== 'map' && (
-          <Link
-            to={`/${pathToString(mapNode?.filePath || currentNode.filePath)}`}
-            key={currentNode.nodeId}
-          >
-            <div className="breadcrumb-item-wrapper">
-              <div
-                className={'breadcrumb-item'}
-                onClick={() => setSelectedNode(mapNode || currentNode)}
-              >
-                <div className="item-icon">
-                  <RiMapPinLine />
-                </div>
-                {pinIdsToPinsMap[currentNode.pinId]?.title || 'Pin'}
-              </div>
-              <RiArrowRightSLine />
-            </div>
-          </Link>
-        )}
-      </div>
       <div key={currentNode.nodeId} className={'breadcrumb-item selected'}>
         <div className="item-icon">
           {currentNode.type === 'text' && <RiStickyNoteLine />}
